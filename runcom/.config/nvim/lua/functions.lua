7fd1fc67931b44c17a6ae5dd94e47043084649ff
Aaron Gonzalez <a.gonzalez979@gmail.com>
2022-02-14 10:25:51 -0500

    git files not find files

--- runcom/.config/nvim/lua/functions.lua
+++ runcom/.config/nvim/lua/functions.lua
@@ -12,17 +12,17 @@ end
 
 M.workon_curator = function()
     vim.cmd [[:tabnew | tcd /opt/dotdash/dataops/curator/]]
-    require('telescope.builtin').find_files({ prompt_title = '~ Curator ~', cwd = '/opt/dotdash/dataops/curator/' })
+    require('telescope.builtin').git_files({ prompt_title = '~ Curator ~', cwd = '/opt/dotdash/dataops/curator/' })
 end
 
 M.workon_aqueduct = function()
     vim.cmd [[:tabnew | tcd /opt/dotdash/dataops/aqueduct/]]
-    require('telescope.builtin').find_files({ prompt_title = '~ Aqueduct ~', cwd = '/opt/dotdash/dataops/aqueduct/' })
+    require('telescope.builtin').git_files({ prompt_title = '~ Aqueduct ~', cwd = '/opt/dotdash/dataops/aqueduct/' })
 end
 
 M.workon_dotfiles = function()
     vim.cmd [[:tabnew | tcd /opt/dotdash/dataops/dotfiles/]]
-    require('telescope.builtin').find_files({ prompt_title = '~ work dotfiles ~', cwd = '/opt/dotdash/dataops/dotfiles/' })
+    require('telescope.builtin').git_files({ prompt_title = '~ work dotfiles ~', cwd = '/opt/dotdash/dataops/dotfiles/' })
 end
 
 M.python_run = function()
