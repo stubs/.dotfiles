430a39c7fab5544d4391ce5d1786ae6be4b555a4
Aaron Gonzalez <a.gonzalez979@gmail.com>
2024-02-07 20:51:04 -0500

    refactored init.lua and plugins

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,7 +5,34 @@ autocmd ColorScheme * highlight ExtraWhitespace ctermbg=red guibg=red
 au InsertLeave * match ExtraWhitespace /\s\+$/
 ]]
 
-require('plugins')
-require('settings')
-require('key_maps')
-require('functions')
+require("plugins")
+
+-- plugin configs
+require("config/aerial")
+require("config/comment")
+-- require("config/diffview")
+require("config/gitsigns")
+require("config/goto-preview")
+require("config/lualine")
+require("config/luasnip")
+require("config/luatab")
+require("config/mason") -- mason before nvim-lspconfig required
+require("config/null-ls")
+require("config/nvim-cmp")
+require("config/nvim-dap")
+require("config/nvim-dap-python")
+require("config/nvim-dap-ui")
+require("config/nvim-lspconfig")
+require("config/nvim-lspconfig")
+require("config/nvim-treesitter")
+require("config/oil")
+require("config/sg")
+require("config/telescope")
+require("config/todo-comments")
+require("config/trouble")
+require("config/venv-selector")
+-- require("config/yaml-companion")
+
+require("settings")
+require("key_maps")
+require("functions")
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,8 +1,8 @@
 local fn = vim.fn
 
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data").."/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-    packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    packer_bootstrap = fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
 end
 
 -- autocmd to sync deps when this file is written to.
@@ -13,153 +13,46 @@ autocmd BufWritePost plugins.lua source <afile> | source $MYVIMRC | PackerSync
 augroup end
 ]])
 
-return require('packer').startup(function(use)
-    -- My plugins here
-  use {'Raimondi/delimitMate'}
-  use {'morhetz/gruvbox'}
-  use {'stevearc/aerial.nvim'}    -- setup in key_maps with on_attach
-  use {'tpope/vim-fugitive'}
-  use {'tpope/vim-surround'}
-  use {'wbthomason/packer.nvim'}  -- have packer manage itself
-  use {'nvim-lua/plenary.nvim'}
-  use {
-    'sindrets/diffview.nvim',
-    requires = 'nvim-lua/plenary.nvim',
-    config= function()
-        require('config/diffview')
-    end,
-    disable=true
-  }
-  use {
-    'lewis6991/gitsigns.nvim',
-    requires = { 'nvim-lua/plenary.nvim' },
-    config = function()
-        require("config/gitsigns")
-    end,
-    after = 'nvim-dap',
-    disable=false
-  }
-  use {
-     'mfussenegger/nvim-dap' ,
-     config = function()
-         require("config/nvim-dap")
-     end,
-    disable=false
-  }
-  use {
-    'rcarriga/nvim-dap-ui',
-    requires  = { 'mfussenegger/nvim-dap' },
-    config = function()
-        require("config/nvim-dap-ui")
-    end,
-    disable=false
-  }
-  use {
-    'mfussenegger/nvim-dap-python',
-    requires  = { 'mfussenegger/nvim-dap' },
-     config = function()
-        require('config/nvim-dap-python')
-      end,
-    disable=false
-  }
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make'}
-  use {
-    'rmagatti/goto-preview',
-    config = function()
-        require('config/goto-preview')
-    end,
-    disable=false
-  }
-  use {
-    'numToStr/Comment.nvim',
-    config = function()
-        require('Comment').setup()
-    end,
-    disable=false
-  }
-  use {
-    'L3MON4D3/LuaSnip',
-    -- after = 'nvim-cmp',
-    config = function ()
-        require('config/luasnip')
-    end,
-    disable=false
-  }
-  use {
-    'hrsh7th/nvim-cmp',
-    requires = { 'hrsh7th/cmp-buffer', 'hrsh7th/cmp-path', 'hrsh7th/cmp-cmdline', 'hrsh7th/cmp-nvim-lsp', 'saadparwaiz1/cmp_luasnip', 'rafamadriz/friendly-snippets' },
-    config = function()
-        require('config/nvim-cmp')
-    end,
-    disable=false
-  }
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    config = function()
-        require("config/nvim-treesitter")
-    end,
-    run = ':TSUpdate',
-    disable=false
-  }
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = 'nvim-tree/nvim-web-devicons',
-    config = function()
-        require("config/lualine")
-    end
-  }
-  use {
-    'folke/trouble.nvim',
-    requires = 'nvim-tree/nvim-web-devicons',
-    config = function()
-        require("config/trouble")
-    end,
-    disable=false
-  }
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = {'nvim-lua/plenary.nvim'},
-    config = function()
-        require("config/telescope")
-    end,
-    disable=false
-  }
-  use {
-    'neovim/nvim-lspconfig', -- Collection of configurations for the built-in LSP client
-    run = function()
-        require("config/nvim-lspconfig")
-    end,
-    disable=false
-  }
-  use {
-    'jose-elias-alvarez/null-ls.nvim',
-    requires = { 'nvim-lua/plenary.nvim' },
-    config = function()
-        require("config/null-ls")
-    end,
-    disable=false
-  }
-  use {
-    'alvarosevilla95/luatab.nvim',
-    requires='nvim-tree/nvim-web-devicons',
-    config = function()
-        require("config/luatab")
-    end
-  }
-  use {
-    'rebelot/kanagawa.nvim'
-  }
-  use {
-    'simrat39/rust-tools.nvim',
-    disable=true
-  }
-  use {
-    'github/copilot.vim',
-    disable=true
-  }
+return require("packer").startup(function(use)
+    use {"L3MON4D3/LuaSnip"}
+    use {"Raimondi/delimitMate"}
+    use {"alvarosevilla95/luatab.nvim", requires = "nvim-tree/nvim-web-devicons"}
+    use {"folke/todo-comments.nvim", requires = "nvim-lua/plenary.nvim"}
+    use {"folke/trouble.nvim", requires = "nvim-tree/nvim-web-devicons"}
+    use {"folke/twilight.nvim"}
+    use {"github/copilot.vim", disable = true}
+    use {"hrsh7th/nvim-cmp", requires = {"hrsh7th/cmp-buffer", "hrsh7th/cmp-path", "hrsh7th/cmp-cmdline", "hrsh7th/cmp-nvim-lsp", "saadparwaiz1/cmp_luasnip", "rafamadriz/friendly-snippets"}}
+    use {"jose-elias-alvarez/null-ls.nvim", requires = "nvim-lua/plenary.nvim"}
+    use {"lewis6991/gitsigns.nvim", requires = "nvim-lua/plenary.nvim"}
+    use {"linux-cultist/venv-selector.nvim", requires = {"neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python"}}
+    use {"mfussenegger/nvim-dap"}
+    use {"mfussenegger/nvim-dap-python", requires  = "mfussenegger/nvim-dap"}
+    use {"morhetz/gruvbox"}
+    use {"neovim/nvim-lspconfig"}
+    use {"numToStr/Comment.nvim"}
+    use {"nvim-lua/plenary.nvim"}
+    use {"nvim-lualine/lualine.nvim", requires = "nvim-tree/nvim-web-devicons"}
+    use {"nvim-telescope/telescope-fzf-native.nvim", run = "make"}
+    use {"nvim-telescope/telescope.nvim", requires = "nvim-lua/plenary.nvim"}
+    use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
+    use {"rcarriga/nvim-dap-ui", requires  = "mfussenegger/nvim-dap"}
+    use {"rebelot/kanagawa.nvim"}
+    use {"rmagatti/goto-preview"}
+    use {"simrat39/rust-tools.nvim", disable = true}
+    use {"sindrets/diffview.nvim", requires = "nvim-lua/plenary.nvim", disable = true}
+    use {"someone-stole-my-name/yaml-companion.nvim", requires = {"neovim/nvim-lspconfig", "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim"}}
+    use {"sourcegraph/sg.nvim", run = "nvim -l build/init.lua"}
+    use {"stevearc/aerial.nvim"}
+    use {"stevearc/oil.nvim"}
+    use {"tpope/vim-fugitive"}
+    use {"tpope/vim-surround"}
+    use {"wbthomason/packer.nvim"}  -- have packer manage itself
+    use {"williamboman/mason-lspconfig.nvim"}
+    use {"williamboman/mason.nvim"}
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
-    require('packer').sync()
+    require("packer").sync()
   end
 end)
