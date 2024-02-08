a804e21606c01580a6f601f8a02a6ed357cbb80a
Aaron Gonzalez <a.gonzalez979@gmail.com>
2024-02-07 21:25:07 -0500

    config: add hadolint and selene linters

--- nvim/.config/nvim/lua/config/null-ls.lua
+++ nvim/.config/nvim/lua/config/null-ls.lua
@@ -1,7 +1,8 @@
-require('null-ls').setup({
+require("null-ls").setup({
     sources = {
-        require('null-ls').builtins.diagnostics.shellcheck,
-        require('null-ls').builtins.diagnostics.flake8
-        -- require('null-ls').builtins.formatting.stylua
+        require("null-ls").builtins.diagnostics.shellcheck,
+        require("null-ls").builtins.diagnostics.hadolint,
+        require("null_ls").builtins.diagnostics.selene
+        -- require("null-ls").builtins.diagnostics.flake8
     }
 })
