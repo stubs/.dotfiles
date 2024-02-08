5a7af415824a77e13af6b7605a0427a388ede25a
Aaron Gonzalez <a.gonzalez979@gmail.com>
2024-02-07 21:17:27 -0500

    config: added yaml-companion config

---
+++ nvim/.config/nvim/lua/config/yaml-companion.lua
@@ -0,0 +1,39 @@
+-- local cfg = require("yaml-companion").setup({
+--   -- Built in file matchers
+--   builtin_matchers = {
+--     -- Detects Kubernetes files based on content
+--     kubernetes = { enabled = true },
+--     cloud_init = { enabled = true }
+--   },
+--
+--   -- Additional schemas available in Telescope picker
+--   schemas = {
+--     {
+--         name = "dbt yml files",
+--         uri = "https://raw.githubusercontent.com/dbt-labs/dbt-jsonschema/main/schemas/dbt_yml_files.json",
+--     },
+--   },
+--
+--   -- Pass any additional options that will be merged in the final LSP config
+--   lspconfig = {
+--     flags = {
+--       debounce_text_changes = 150,
+--     },
+--     settings = {
+--       redhat = { telemetry = { enabled = false } },
+--       yaml = {
+--         validate = true,
+--         format = { enable = true },
+--         hover = true,
+--         schemaStore = {
+--           enable = true,
+--           url = "https://www.schemastore.org/api/json/catalog.json",
+--         },
+--         schemaDownload = { enable = true },
+--         schemas = {},
+--         trace = { server = "debug" },
+--       },
+--     },
+--   },
+-- })
+-- require("lspconfig")["yamlls"].setup(cfg)
