3f7594197867de3a39c52d9df63b217e6f3d8900
Aaron Gonzalez <a.gonzalez979@gmail.com>
2024-02-08 14:24:40 -0500

    config: missing comma in null-ls

--- nvim/.config/nvim/lua/config/null-ls.lua
+++ nvim/.config/nvim/lua/config/null-ls.lua
@@ -2,7 +2,11 @@ require("null-ls").setup({
     sources = {
         require("null-ls").builtins.diagnostics.shellcheck,
         require("null-ls").builtins.diagnostics.hadolint,
-        require("null-ls").builtins.diagnostics.selene
+        require("null-ls").builtins.diagnostics.selene,
+        require("null-ls").builtins.diagnostics.checkmake,
+        -- require("null-ls").builtins.diagnostics.sqlfluff.with({
+        --     extra_args = { "--config", "PATH_TO_CONFIG" }
+        -- }),
         -- require("null-ls").builtins.diagnostics.flake8
     }
 })
