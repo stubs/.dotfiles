ce897388e2993a5295bf44e499465ab96cc368db
Aaron Gonzalez <a.gonzalez979@gmail.com>
2026-09-11 19:54:35 -0400

    feat(nvim): switching my venvs to uv style.

--- nvim/.config/nvim/lua/config/venv-selector.lua
+++ nvim/.config/nvim/lua/config/venv-selector.lua
@@ -3,7 +3,8 @@ require('venv-selector').setup({
         search = {
             cwd = false,
             virtualenvs = {
-                command = "fd '/bin/python$' ~/.virtualenvs/*/bin --full-path -a",
+                -- command = "fd '/bin/python$' ~/.virtualenvs/*/bin --full-path -a",
+                command = " fd '/bin/python$' .venv --full-path -a -H -I"
             }
         },
     },
